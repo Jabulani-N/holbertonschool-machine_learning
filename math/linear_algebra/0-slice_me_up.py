6733bc32daad2b5aa7d8848eab79428eab88371c
Jabulani-N <jabu.94@gmail.com>
2023-09-07 23:03:03 -0500

    corrected array index range, as starting number is not inclusive

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -2,7 +2,7 @@
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 =  arr[:2]#  keep calm and remember what a matrix/array is in computer work
 arr2 =  arr[-5:]#  you actually already know this
-arr3 =  arr[2:7]#  don't overthink it
+arr3 =  arr[1:7]#  don't overthink it
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
