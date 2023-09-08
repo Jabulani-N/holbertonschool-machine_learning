c1c9c1d84e7bece8ea803eae72fea2862a5a9dfc
Jabulani-N <jabu.94@gmail.com>
2023-09-07 22:23:08 -0500

    added task0 to commit

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 =  arr[:2]# your code here keep calm and remember what a matrix/array is in computer work
-arr2 =  arr[:-5]# your code here you actually already know this
-arr3 =  arr[:]# your code here don't overthink it
+arr1 =  arr[:2]#  keep calm and remember what a matrix/array is in computer work
+arr2 =  arr[-5:]#  you actually already know this
+arr3 =  arr[2:6]#  don't overthink it
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
