52eabe8de980eb1fac92e3d0640ae4c8eb2157c3
Jabulani-N <jabu.94@gmail.com>
2023-09-08 03:31:23 -0500

    created task4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""this module coantains the add_arrays function"""
+
+
+def add_arrays(arr1, arr2):
+    """ adds array elements
+    assume bot arr's are same shape
+    and filled with int/float
+    """
+
+    sumMatrix = []
+
+    return sumMatrix
