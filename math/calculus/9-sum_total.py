fc0ff0b9dc64238729d74f4a3e3dc46ef116f45a
Jabulani-N <jabu.94@gmail.com>
2023-09-12 00:28:53 -0500

    created task9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""this module coantains the summation_i_squared function"""
+
+
+def summation_i_squared(n):
+    """ calculates the summation of i squared
+    for i = 0 up to n
+    """
+
+    sumTotal = 0
+
+    return sumTotal
