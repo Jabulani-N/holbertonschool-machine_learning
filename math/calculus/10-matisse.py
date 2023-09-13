2f1f291c714f27a68773d37ab30ea2f6b93dc9c6
Jabulani-N <jabu.94@gmail.com>
2023-09-12 19:16:58 -0500

    task10: created outline

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,5 +3,17 @@
 
 
 def poly_derivative(poly):
-    """ documentation content"""
-    pass
+    """ derives polynomial represented by list poly
+    with respect to x.
+    each posioitn is a power of x
+    for example: [1, 3, 5] = 1x^2 + 3x^1 + 5x^0
+    remember that x^0 = 1, so 5x^0 == 5
+    """
+
+    if isinstance(poly, list) is False or\
+       len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    derivedPoly = []
+
