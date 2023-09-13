f3fa1093bac93f670954d93be77e7895755e2fb6
Jabulani-N <jabu.94@gmail.com>
2023-09-12 19:45:07 -0500

    task10: corrected for incorrectly assuming input's order. I had it reversed

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,7 +6,7 @@ def poly_derivative(poly):
     """ derives polynomial represented by list poly
     with respect to x.
     each posioitn is a power of x
-    for example: [1, 3, 5] = 1x^2 + 3x^1 + 5x^0
+    for example: [1, 3, 5] = 1x^0 + 3x^1 + 5x^2
     remember that x^0 = 1, so 5x^0 == 5
     """
 
@@ -16,6 +16,7 @@ def poly_derivative(poly):
     if len(poly) == 1:
         return [0]
     derivedPoly = []
+    poly.revrese()
     for position in range(len(poly - 1)):
         derivedPoly.append[poly[position] * (len(poly) - 1 - position)]
 
