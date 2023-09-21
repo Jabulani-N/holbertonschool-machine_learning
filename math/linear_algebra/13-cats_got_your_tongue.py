b2e083a7c19f61d225c11edec4e9c8a392b844a0
Jabulani-N <jabu.94@gmail.com>
2023-09-21 06:48:55 -0500

    task13: style improvement and documentation improvement

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -2,5 +2,9 @@
 """import is permitted"""
 import numpy as np
 
+
 def np_cat(mat1, mat2, axis=0):
+    """concatenates arrays.
+    numpy can concatenate any nyumber of arrays this way
+    """
     return np.concatenate((mat1, mat2), axis)
