c27e3a11c0d0d524eef1ea88f8e54c2f8de1cfe0
Jabulani-N <jabu.94@gmail.com>
2023-09-20 20:57:37 -0500

    task3: created

---
+++ math/probability/exponential.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""this module coantains the exponential class"""
+
+
+class Exponential:
+    """represents exponential distribution of data"""
+
+    def __init__(self, data=None, lambtha=1.):
+        pass
