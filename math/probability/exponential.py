9b237aaaef54887f8dffa4493040d79e35171a94
Jabulani-N <jabu.94@gmail.com>
2023-09-20 21:52:12 -0500

    task4: populated method pdf with formula

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """this module coantains the exponential class"""
-
+e = 2.7182818285
+pi = 3.1415926536
 
 class Exponential:
     """represents exponential distribution of data
@@ -46,4 +47,6 @@ class Exponential:
         Returns the PDF value for x
         If x is out of range, return 0
         """
-        pass
+        if x <= 0:
+            return 0
+        return self.lambtha * e ** (-1 * self.lambtha * x)
