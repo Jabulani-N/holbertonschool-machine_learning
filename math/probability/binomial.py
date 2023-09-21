dcd27204ff0d084c198938e9e2b6a6b5828160b7
Jabulani-N <jabu.94@gmail.com>
2023-09-20 22:10:59 -0500

    created binomial class file

---
+++ math/probability/binomial.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""this module coantains the binomial class"""
+e = 2.7182818285
+pi = 3.1415926536
+
+
+class Binomial:
+    """ represents Binomial distribution"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        pass
