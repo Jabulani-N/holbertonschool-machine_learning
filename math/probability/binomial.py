527d93db232b6700021b12b612069d3125224fa5
Jabulani-N <jabu.94@gmail.com>
2023-09-21 09:17:38 -0500

    evaluates n and p values for validity

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -5,7 +5,26 @@ pi = 3.1415926536
 
 
 class Binomial:
-    """ represents Binomial distribution"""
+    """ represents Binomial distribution
+    calculate p first, then n, then recalculate p
+    """
 
     def __init__(self, data=None, n=1, p=0.5):
-        pass
+        if n <= 0:
+            raise ValueError("n must be a positive value")
+        else:
+            self.n = int(n)
+
+        if p < 0 or p > 1:
+            raise ValueError("p must be greater than 0 and less than 1")
+        self.p = float(p)
+
+        if isinstance(data, list) is False and\
+                data is not None:
+            raise TypeError("data must be a list")
+        elif isinstance(data, list) and\
+                len(data) < 2:
+            raise\
+                ValueError("data must contain multiple values")
+        elif data is not None:
+            pass  # where p & n would be calculated
