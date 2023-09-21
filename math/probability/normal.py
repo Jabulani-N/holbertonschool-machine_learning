da5e181d8bd12b263a49743cd3d0ff09446244fa
Jabulani-N <jabu.94@gmail.com>
2023-09-20 22:09:52 -0500

    created normal class file

---
+++ math/probability/normal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""this module coantains the normal class"""
+e = 2.7182818285
+pi = 3.1415926536
+
+
+class Normal:
+    """ represents normal distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        pass
