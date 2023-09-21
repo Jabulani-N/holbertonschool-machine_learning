7122e32e0e3b18e12593b8693f632e68ae164900
Jabulani-N <jabu.94@gmail.com>
2023-09-20 23:33:23 -0500

    task6: calculates self.mean

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -7,5 +7,19 @@ pi = 3.1415926536
 class Normal:
     """ represents normal distribution"""
 
-    def __init__(self, data=None, lambtha=1.):
-        pass
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if stddev <= 0:
+            raise ValueError("stddev must be a positive value")
+        else:
+            self.stddev = float(stddev)
+        self.mean = mean
+
+        if isinstance(data, list) is False and\
+                data is not None:
+            raise TypeError("data must be a list")
+        elif isinstance(data, list) and\
+                len(data) < 2:
+            raise\
+                ValueError("data must contain multiple values")
+        elif data is not None:
+            self.mean = (sum(data) / len(data))  # mean is average
