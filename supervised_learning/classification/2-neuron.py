1d99bd3171474864d9ee01cb7effa38fc7c2188e
Jabulani-N <jabu.94@gmail.com>
2023-10-04 21:30:54 -0500

    created and documented forward_prop method

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -50,3 +50,16 @@ class Neuron:
     def A(self):
         """getter"""
         return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+        X is numpy.ndarray of shape (nx, m) & contains the input data
+            nx is the number of input features to the neuron
+            m is the number of examples
+
+        Updates the private attribute __A
+            The neuron should use a sigmoid activation function
+        
+        Returns the private attribute __A
+        """
+        pass
