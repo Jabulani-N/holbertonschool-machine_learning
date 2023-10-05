50b5a89c0f1ea0286501f1d687c29f66ade6a23c
Jabulani-N <jabu.94@gmail.com>
2023-10-05 11:45:44 -0500

    documented task5

--- supervised_learning/classification/5-neuron.py
+++ supervised_learning/classification/5-neuron.py
@@ -62,4 +62,60 @@ class Neuron:
 
         Returns the private attribute __A
         """
+        # I need to multiply everything in X array by the weight
+        # weight is self.W()
+        # weighted_X = X * 1
+        # above is where I need to multiply x by wieghts
+        self.__A = self.sigmoid(np.matmul(self.__W, X) + self.__b)
+        return self.__A
+
+    def sigmoid(self, x):
+        """sigmoid function used in propagation"""
+        sig = 1 / (1 + np.exp(-1 * x))
+        return sig
+
+    def cost(self, Y, A):
+        """determines cost
+        uses
+            1.0000001 - A
+        anytime there would otherwise be a
+            1 - A
+        Y is numpy.ndarray
+            with shape (1, m)
+            contains the correct labels for the input data
+        A is a numpy.ndarray
+            with shape (1, m)
+            containing the activated output of the neuron for each example
+        """
+        m = Y.shape[1]
+        # need to multiple -1/m by sum
+        loss = (-1 / m) * np.sum(Y *
+                                 np.log(A) + ((1 - Y) *
+                                              np.log(1.0000001 - A)))
+        return loss
+
+    def evaluate(self, X, Y):
+        """evaluates the neuron's predictions
+        X is a numpy.ndarray
+            shape (nx, m)
+            contains the input data
+            nx is the number of input features to the neuron
+            m is the number of examples
+        Y is numpy.ndarray
+            with shape (1, m)
+            contains the correct labels for the input data
+        """
+        # expect to use np.where
+        A = self.forward_prop(X)
+        prediction = np.where(A >= 0.5, 1, 0)
+        loss = self.cost(Y, A)
+        return prediction, loss
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """does a single pass of gradient descent
+        alpha = learning rate = weird looking loop
+        J(w,b) = loss function
+        new w = w - alpha * (derivative of J(wb) respect w)
+        new b = b - alpha * (derivative of J(wb) respect b)
+        """
         pass
